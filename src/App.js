eeb591162039533f0809d60397b1b9d5244da18f
Om Tiwari <omtiwari9755@gmail.com>
2025-10-19 12:53:00 +0530

    installing code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button/Button";
+import Logo from "./components/Logo/Logo";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
+
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from 'react';
+
+import {
+  fetchNewAlbum,
+  fetchTopAlbum,
+  fetchFilters,
+  fetchSongs,
+} from "./components/api/api";
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  const generateData = (key, dataSource) => {
+    dataSource().then((data) => {
+      setData((prevData) => {
+        return {...prevData, [key]: data};
+      });
+    });
+  };
+
+const { topAlbum = [], newAlbum = [], genres = [], songs = [] } = data;
+
+
+  useEffect(() => {
+    generateData("topAlbum", fetchTopAlbum);
+    generateData("newAblum", fetchNewAlbum);
+    generateData("genres", fetchFilters);
+    generateData("songs", fetchSongs);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar searchData = {[...topAlbum, newAlbum]}>
+        <Logo />
+        <Search placeholder={"Search a album of your choice"} />
+        <Button 
+          text="Give Feedback"
+          onClick={() => alert("Feedback button clicked")}
+        />
+      </Navbar>
+      <Outlet context={{data: { topAlbum, newAlbum, genres, songs } }} />
+    </>
   );
 }
 
